9586e0fa3a524608586f82fc665f86c4e39b1200
Thallyta Castro <thallyta180136319@gmail.com>
2021-11-28 18:03:02 -0300

    Inclusão de novos desafios resolvidos

---
+++ C#/TrigoNoTabuleiro.cs
@@ -0,0 +1,35 @@
+/* Desafio
+using System;
+using System.Numerics;
+
+class Program {
+  static void Main(string[] args) { 
+    int n = int.Parse(Console.ReadLine());
+    ulong x;
+    BigInteger totalGrams;
+    
+    for (int i = 0; i < n; ++i) {
+      x = ulong.Parse(Console.ReadLine()); 
+      totalGrams = new BigInteger((Math.Pow(2, x) / (12 * 1000)));
+      Console.WriteLine($"{totalGrams} kg");
+    }
+  }
+}
+ */
+
+using System;
+using System.Numerics;
+
+class Program {
+  static void Main(string[] args) { 
+    int n = int.Parse(Console.ReadLine());
+    ulong x;
+    BigInteger totalGrams;
+    
+    for (int i = 0; i < n; ++i) {
+      x = ulong.Parse(Console.ReadLine()); 
+      totalGrams = new BigInteger((Math.Pow(2, x) / (12 * 1000)));
+      Console.WriteLine($"{totalGrams} kg");
+    }
+  }
+}
